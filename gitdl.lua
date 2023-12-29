2dd6f659a2dcb53d14a80188534aa7336b0a6c25
ahito <dfox.kiev@gmail.com>
2023-12-29 11:59:38 +0200

    Added `gitdl`

---
+++ gitdl.lua
@@ -0,0 +1,28 @@
+args = {...}
+local gitraw_url = "https://raw.githubusercontent.com/ahito/cc/"
+local filename = ""
+local branch = "main"
+if #args == 1 then
+	filename = args[1]
+elseif #args == 2 then
+	branch = args
+	filename = args[2]
+else
+	print("Usage:")
+	print("gitdl <filename>")
+	print("gitdl <branch> <filename>")
+	
+	error()
+end
+
+local url = gitraw_url..branch.."/"..filename
+print(url)
+local request = http.get(url)
+if request.getResponseCode() == 200 then
+	file = fs.open(filename,"w")
+	file.write(request.readAll())
+	file.close()
+	print("File '"..filename.."' loaded.\n")
+else
+	print("Failed")
+end
