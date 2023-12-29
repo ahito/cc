5493829bb14a69ab6fc35717a152e7bd0025d9dd
ahito <dfox.kiev@gmail.com>
2023-12-29 12:03:38 +0200

    Fixed gitdl

--- gitdl.lua
+++ gitdl.lua
@@ -5,7 +5,7 @@ local branch = "main"
 if #args == 1 then
 	filename = args[1]
 elseif #args == 2 then
-	branch = args
+	branch = args[1]
 	filename = args[2]
 else
 	print("Usage:")
@@ -18,7 +18,7 @@ end
 local url = gitraw_url..branch.."/"..filename
 print(url)
 local request = http.get(url)
-if request.getResponseCode() == 200 then
+if request ~= nil and request.getResponseCode() == 200 then
 	file = fs.open(filename,"w")
 	file.write(request.readAll())
 	file.close()
