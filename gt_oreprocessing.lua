9671a4c5625ade1400608dbc3ada32b7c2a8277e
ahito <dfox.kiev@gmail.com>
2023-12-29 17:57:47 +0200

    Added basic recipe processing logic

--- gt_oreprocessing.lua
+++ gt_oreprocessing.lua
@@ -1,9 +1,17 @@
+require("adebugutils")
+term.clear()
 local recipes = {}
+local drawer = peripheral.find("functionalstorage:storage_controller")
+if drawer == nil then 
+	error("No \"functionalstorage:storage_controller\" found in network.") 
+end
 local targets = {
-	["drawer"] = peripheral.find("functionalstorage:storage_controller"),
 	["packer"] = peripheral.find("gtceu:mv_packer"),
 	["centrifuge"] = peripheral.find("gtceu:mv_centrifuge"),
 	["electrolyzer"] = peripheral.find("gtceu:mv_electrolyzer")
+}
+local status = {
+	["last_jobs"] = {}
 }
 function AddRecipe(InTarget,InName,InRecipeDescriptor)
 	if targets[InTarget] == nil then
@@ -20,8 +28,16 @@ function AddRecipe(InTarget,InName,InRecipeDescriptor)
 	end
 	print("Added recipe \"" .. InName .. "\" for \"" .. InTarget .. "\"")
 end
-function IsTargetEmpty(Target)
-	if Target == nil or Target.list == nil then return false end
+function IsValidTarget(Target)
+	if 
+		Target == nil
+		or type(Target) ~= "table"
+		or getmetatable(Target).__name ~= "peripheral" 
+		or getmetatable(Target).types == nil 
+		or not getmetatable(Target).types.inventory 
+	then 
+		return false 
+	end
 	for k,v in pairs(Target.list()) do
 		if v.name ~= "gtceu:programmed_circuit" then
 			return false
@@ -29,6 +45,23 @@ function IsTargetEmpty(Target)
 	end
 	return true
 end
+function MoveItemsToTarget(Target,Ingredients,Multiplier)
+	--has to return true on successful transfer
+	--{slot = item_slot, count = item_short.count}
+
+	if Target == nil then return false end
+	if #Ingredients == 0 then return false end
+
+	for k,v in pairs(Ingredients) do
+		drawer.pushItems(peripheral.getName(Target),v.slot,v.count*Multiplier)
+	end
+	return true
+end
+function DisplayStatus()
+	term.clear()
+	PrintTable(status)
+end
+
 print("Targets validation:")
 for k,v in pairs(targets) do
 	local invalid = true
@@ -52,11 +85,73 @@ for k,v in pairs(targets) do
 	end
 end
 
-AddRecipe("packer", "Tiny dusts", {{key = "forge:tiny_dusts", count = 9}})
-AddRecipe("packer", "Small dusts", {{key = "forge:tiny_dusts", count = 9}})
-AddRecipe("centrifuge", "Rare Earth Dust", {{key = "gtceu:rare_earth_dust", count = 1}})
-AddRecipe("electrolyzer", "Phosphate Dust", {{key = "gtceu:phosphate_dust", count = 5}})
-AddRecipe("electrolyzer", "Apatite Dust", {{key = "gtceu:apatite_dust", count = 5}})
-AddRecipe("foo", "bar", {{key = "minecraft:stone", count = 1}})
+-- RECIPES DECLARATION
+AddRecipe("packer", "tiny dusts compression", {{key = "forge:tiny_dusts", count = 9}})
+AddRecipe("packer", "small dusts compression", {{key = "forge:tiny_dusts", count = 9}})
 
+AddRecipe("centrifuge", "Rare Earth Dust processing", {{key = "gtceu:rare_earth_dust", count = 1}})
+AddRecipe("centrifuge", "Tricalcium Phosphate Dust processing", {{key = "gtceu:tricalcium_phosphate_dust", count = 5}})
 
+AddRecipe("electrolyzer", "Phosphate Dust processing", {{key = "forge:dusts/phosphate", count = 5}})
+AddRecipe("electrolyzer", "Apatite Dust processing", {{key = "gtceu:apatite_dust", count = 5}})
+AddRecipe("electrolyzer", "Rock Salt Dust processing", {{key = "gtceu:rock_salt_dust", count = 2}})
+--AddRecipe("foo", "bar", {{key = "minecraft:stone", count = 1}})
+
+sleep(1)
+
+while true do
+
+	-- iterating target recipe groups
+	for target_alias, target_recipes in pairs(recipes) do
+		local target = targets[target_alias]
+		if IsValidTarget(target) then 
+		-- iterating recipes
+			for recipe_n, recipe in pairs(target_recipes) do
+				local ingredient_slots = {}
+				local has_all_ingredients = true
+				local multiplier = 64
+				
+				--iterating ingredients
+				for ingredient_n,ingredient in pairs(recipe.ingredients) do
+					local ingredient_slot_info = nil
+					
+					-- iterating drawer slots
+					for item_slot, item_short in pairs(drawer.list()) do
+						if item_short.count > ingredient.count then 
+							if item_short.name == ingredient.key  then
+								ingredient_slot_info = {slot = item_slot, count = item_short.count}
+								break
+							else
+								local item_long = drawer.getItemDetail(item_slot)
+								if item_long.tags[ingredient.key] ~= nil then
+									ingredient_slot_info = {slot = item_slot, count = item_short.count}
+									break
+								end
+							end
+						end
+					end
+					if ingredient_slot_info == nil then 
+						has_all_ingredients = false 
+						break 
+					end
+					--Descriptive `multiplier` calculations:
+					--local clamped_count = math.min(ingredient_slot_info.count,64)
+					--local ingredient_max_multiplier = math.floor(clamped_count/ingredient.count)
+					--multiplier = math.min(ingredient_max_multiplier,multiplier)
+					--Short `multiplier` calculations:
+					multiplier = math.min(multiplier,math.floor(math.min(ingredient_slot_info.count,64)/ingredient.count))
+					table.insert(ingredient_slots,ingredient_slot_info)
+				end
+				if has_all_ingredients then
+					local result = MoveItemsToTarget(target,ingredient_slots,multiplier)
+					if result then
+						status.last_jobs[target_alias] = recipe.name
+					end
+					break
+				end
+			end
+		end
+	end	
+	DisplayStatus()
+	sleep(1)
+end
